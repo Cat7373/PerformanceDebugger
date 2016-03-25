f647f7de1193db65afec790d961f3de7ea2f675b
Cat73 <1901803382@qq.com>
2016-03-25 11:51:16 +0800

    不再对高于 21 的 TPS 进行过滤

--- src/org/cat73/performancedebugger/task/tasks/CalculationTPSTask.java
+++ src/org/cat73/performancedebugger/task/tasks/CalculationTPSTask.java
@@ -4,9 +4,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 import org.cat73.performancedebugger.task.ITask;
 
+/**
+ * 计算 TPS 的 Task
+ *
+ * @author cat73
+ */
 public class CalculationTPSTask implements ITask {
     /** 上次 Task 被主线程执行的时间 */
-    private static long lastPoll = System.nanoTime();
+    private static long lastPoll;
     /** 最后一次统计到的 TPS */
     private static double lastTPS = 20.0D;
     /** Task ID */
@@ -24,6 +29,7 @@ public class CalculationTPSTask implements ITask {
     @Override
     public void start(final BukkitScheduler scheduler, final JavaPlugin javaPlugin) {
         // 启动 Task
+        CalculationTPSTask.lastPoll = System.nanoTime();
         this.taskID = scheduler.scheduleSyncRepeatingTask(javaPlugin, this, 100, 100);
     }
 
@@ -41,19 +47,17 @@ public class CalculationTPSTask implements ITask {
         final long startTime = System.nanoTime();
 
         // 计算时间间隔
-        long timeSpent = (startTime - CalculationTPSTask.lastPoll) / 1000L;
-        if (timeSpent == 0L) {
-            timeSpent = 1L;
-        }
-
-        // 计算 TPS
-        final double tps = 1.0E8D / timeSpent;
-        if (tps <= 21.0D) {
-            // 保存本次统计到的 TPS
-            CalculationTPSTask.lastTPS = tps;
-        }
+        final long timeSpent = startTime - CalculationTPSTask.lastPoll;
 
         // 保存本次 Task 被执行的时间
         CalculationTPSTask.lastPoll = startTime;
+
+        // 防止除 0 错误(虽然基本不可能出现)
+        if (timeSpent == 0L) {
+            return;
+        }
+
+        // 计算并保存 TPS
+        CalculationTPSTask.lastTPS = 1.0E11D / timeSpent;
     }
 }
